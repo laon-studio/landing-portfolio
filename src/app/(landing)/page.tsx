40b5205872729af946276f1d1b0c5897bed102ff
sojeongsojeong-park <psj90972@naver.com>
2025-05-22 11:28:59 +0900

    ✨first commit

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+	return (
+		<div className=' bg-primary p-4'>
+			<h1 className='text-red-500'>Landing Page</h1>
+			<div className='p-4 bg-blue-200'>패딩 테스트</div>
+		</div>
+	);
+};
+
+export default LandingPage;
