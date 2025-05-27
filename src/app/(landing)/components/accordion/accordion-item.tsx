9a505b1fc72c7117686dd2849fe807c314c1f353
sojeongsojeong-park <psj90972@naver.com>
2025-05-27 21:25:20 +0900

    feat: add transition to accordion component

--- src/app/(landing)/components/accordion/accordion-item.tsx
+++ src/app/(landing)/components/accordion/accordion-item.tsx
@@ -1,13 +1,48 @@
+'use client';
+
+import { useEffect, useReducer, useRef, useState } from 'react';
+import IconArrow from './arrow';
+
 interface IAccordionItem {
 	title: string;
 	content: string;
 }
 
 const AccordionItem = ({ title, content }: IAccordionItem) => {
+	const contentRef = useRef<HTMLDivElement>(null);
+	const [isOpen, setIsOpen] = useReducer((state) => !state, false);
+	const [height, setHeight] = useState('0px');
+
+	useEffect(() => {
+		if (contentRef.current) {
+			setHeight(isOpen ? `${contentRef.current.scrollHeight}px` : '0px');
+		}
+	}, [isOpen]);
+
 	return (
 		<div className='w-full border border-platinum rounded-3xl p-5'>
-			<span>{title}</span>
-			<p>{content}</p>
+			<div
+				className='flex justify-between items-center cursor-pointer group overflow-hidden'
+				onClick={setIsOpen}>
+				<span>{title}</span>
+				<button
+					className={`p-4 ${
+						isOpen ? 'bg-primary' : 'bg-platinum'
+					} group-hover:bg-primary rounded-lg transition-color duration-300`}>
+					<IconArrow isOpen={isOpen} />
+				</button>
+			</div>
+			<div
+				className='transition-all duration-500 ease-in-out overflow-hidden'
+				style={{ height }}>
+				<div
+					ref={contentRef}
+					className={`p-4 transition-all duration-500 ${
+						isOpen ? 'opacity-100' : 'opacity-0'
+					}`}>
+					{content}
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/app/(landing)/components/accordion/arrow.tsx
@@ -0,0 +1,16 @@
+const IconArrow = ({ isOpen }: { isOpen: boolean }) => {
+	return (
+		<svg
+			className={`w-4 h-4 text-black md:w-8 md:h-8 ${
+				isOpen ? 'rotate-180' : ''
+			} transition-transform duration-300`}
+			focusable='false'
+			aria-hidden='true'
+			viewBox='0 0 24 24'
+			data-testid='ArrowDownwardIcon'>
+			<path d='m20 12-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8z'></path>
+		</svg>
+	);
+};
+
+export default IconArrow;
