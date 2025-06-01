8b0851b7388dd3aac47111d2c3cbf0b0ba876580
sojeongsojeong-park <psj90972@naver.com>
2025-06-01 21:47:15 +0900

    chore: set up prettier and format entire codebase

--- src/app/(landing)/components/accordion/accordion-item.tsx
+++ src/app/(landing)/components/accordion/accordion-item.tsx
@@ -4,56 +4,60 @@ import { useEffect, useReducer, useRef, useState } from 'react';
 import IconArrow from './arrow';
 
 interface IAccordionItem {
-	title: string;
-	content: string;
+  title: string;
+  content: string;
 }
 
 const AccordionItem = ({ title, content }: IAccordionItem) => {
-	const contentRef = useRef<HTMLDivElement>(null);
-	const [isOpen, setIsOpen] = useReducer((state) => !state, false);
-	const [height, setHeight] = useState('0px');
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useReducer((state) => !state, false);
+  const [height, setHeight] = useState('0px');
 
-	useEffect(() => {
-		const updateHeight = () => {
-			if (contentRef.current) {
-				const scrollHeight = contentRef.current.scrollHeight;
-				setHeight(isOpen ? `${scrollHeight}px` : '0px');
-			}
-		};
+  useEffect(() => {
+    const updateHeight = () => {
+      if (contentRef.current) {
+        const scrollHeight = contentRef.current.scrollHeight;
+        setHeight(isOpen ? `${scrollHeight}px` : '0px');
+      }
+    };
 
-		updateHeight();
+    updateHeight();
 
-		window.addEventListener('resize', updateHeight);
+    window.addEventListener('resize', updateHeight);
 
-		return () => window.removeEventListener('resize', updateHeight);
-	}, [isOpen]);
+    return () => window.removeEventListener('resize', updateHeight);
+  }, [isOpen]);
 
-	return (
-		<div className='w-full border border-platinum rounded-3xl p-5 md:px-8 md:py-10'>
-			<div
-				className='flex justify-between items-center cursor-pointer group overflow-hidden'
-				onClick={setIsOpen}>
-				<span className='text-xl md:text-2xl'>{title}</span>
-				<button
-					className={`p-1 md:p-3 ${
-						isOpen ? 'bg-primary' : 'bg-platinum'
-					} group-hover:bg-primary rounded-lg transition-color duration-300`}>
-					<IconArrow isOpen={isOpen} />
-				</button>
-			</div>
-			<div
-				className='transition-all duration-500 ease-in-out overflow-hidden'
-				style={{ height }}>
-				<div
-					ref={contentRef}
-					className={`py-3 transition-all duration-500 text-sm ${
-						isOpen ? 'opacity-100' : 'opacity-0'
-					}`}>
-					{content}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='w-full border border-platinum rounded-3xl p-5 md:px-8 md:py-10'>
+      <div
+        className='flex justify-between items-center cursor-pointer group overflow-hidden'
+        onClick={setIsOpen}
+      >
+        <span className='text-xl md:text-2xl'>{title}</span>
+        <button
+          className={`p-1 md:p-3 ${
+            isOpen ? 'bg-primary' : 'bg-platinum'
+          } group-hover:bg-primary rounded-lg transition-color duration-300`}
+        >
+          <IconArrow isOpen={isOpen} />
+        </button>
+      </div>
+      <div
+        className='transition-all duration-500 ease-in-out overflow-hidden'
+        style={{ height }}
+      >
+        <div
+          ref={contentRef}
+          className={`py-3 transition-all duration-500 text-sm ${
+            isOpen ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
+          {content}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AccordionItem;
--- src/app/(landing)/components/accordion/index.tsx
+++ src/app/(landing)/components/accordion/index.tsx
@@ -1,61 +1,61 @@
 import AccordionItem from './accordion-item';
 
 const AccordionContents = [
-	{
-		title: 'Clear transparency',
-		content:
-			'Hifi is a payment platform that allows you to accept payments from your customers.',
-	},
-	{
-		title: 'Privacy and security centered',
-		content:
-			"Our customer’s data and privacy will always be our top priority. We firmly believe in our customers' right to data privacy, and we fully commit to safeguarding it. Utilizing top preventive measures, we actively mitigate data security risks. We aim to serve as a symbol of trust and reliability for best security practices in the new financial world.",
-	},
-	{
-		title: 'Humans first',
-		content:
-			'Everyone deserves a fair share. We listen to our customers and create our products with the intention that they will help the lives of people everyday. We know our products will be used by real humans, each with their own unique lives, thoughts and feelings. Our products are built with empathy to be accessible by everyone, everywhere.',
-	},
-	{
-		title: 'Think big, do bigger',
-		content:
-			'We have big ambitious plans and believe no goal is ever out of reach. We reward big, bold bets and comprise ourselves of a stellar team of dreamers and doers. We take a ‘zero-to-one’ approach and believe the most disruptive tech in the world all starts with just an idea and the firm belief it can be built into something life changing.',
-	},
+  {
+    title: 'Clear transparency',
+    content:
+      'Hifi is a payment platform that allows you to accept payments from your customers.',
+  },
+  {
+    title: 'Privacy and security centered',
+    content:
+      "Our customer’s data and privacy will always be our top priority. We firmly believe in our customers' right to data privacy, and we fully commit to safeguarding it. Utilizing top preventive measures, we actively mitigate data security risks. We aim to serve as a symbol of trust and reliability for best security practices in the new financial world.",
+  },
+  {
+    title: 'Humans first',
+    content:
+      'Everyone deserves a fair share. We listen to our customers and create our products with the intention that they will help the lives of people everyday. We know our products will be used by real humans, each with their own unique lives, thoughts and feelings. Our products are built with empathy to be accessible by everyone, everywhere.',
+  },
+  {
+    title: 'Think big, do bigger',
+    content:
+      'We have big ambitious plans and believe no goal is ever out of reach. We reward big, bold bets and comprise ourselves of a stellar team of dreamers and doers. We take a ‘zero-to-one’ approach and believe the most disruptive tech in the world all starts with just an idea and the firm belief it can be built into something life changing.',
+  },
 ];
 
 const AccordionSection = () => {
-	return (
-		<div className='w-full grid grid-cols-1 md:grid-cols-3'>
-			<TextSection />
-			<div className='w-full max-w-3xl col-span-2 ml-auto'>
-				<div className='flex flex-col'>
-					{AccordionContents.map((item, index) => (
-						<AccordionItem
-							key={index}
-							title={item.title}
-							content={item.content}
-						/>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='w-full grid grid-cols-1 md:grid-cols-3'>
+      <TextSection />
+      <div className='w-full max-w-3xl col-span-2 ml-auto'>
+        <div className='flex flex-col'>
+          {AccordionContents.map((item, index) => (
+            <AccordionItem
+              key={index}
+              title={item.title}
+              content={item.content}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AccordionSection;
 
 const TextSection = () => {
-	return (
-		<div className='col-span-1 flex flex-col gap-5 px-2 py-8 md:gap-8 md:px-8 md:py-10'>
-			<span className='text-xs'>Our Values</span>
-			<h2 className='md:text-4xl text-2xl'>We Believe</h2>
-			<p className='text-sm'>
-				At HiFi, we respect the visionaries that came before us - however, we
-				are taking a radically different approach than those that built the
-				world of big tech and traditional finance. We understand that the path
-				to building a redefined financial future for everyone starts with values
-				we bake into our work every day.
-			</p>
-		</div>
-	);
+  return (
+    <div className='col-span-1 flex flex-col gap-5 px-2 py-8 md:gap-8 md:px-8 md:py-10'>
+      <span className='text-xs'>Our Values</span>
+      <h2 className='md:text-4xl text-2xl'>We Believe</h2>
+      <p className='text-sm'>
+        At HiFi, we respect the visionaries that came before us - however, we
+        are taking a radically different approach than those that built the
+        world of big tech and traditional finance. We understand that the path
+        to building a redefined financial future for everyone starts with values
+        we bake into our work every day.
+      </p>
+    </div>
+  );
 };
