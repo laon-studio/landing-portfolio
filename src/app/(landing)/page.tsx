bb97fc53dd0c976b71686ead6d2a5c75f383848a
iizz <ebiz0427@gmail.com>
2025-06-02 17:54:46 +1000

    feat: create Hero section

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import Header from './components/header';
-import Footer from './components/footer';
+import Hero from './components/hero';
 import AccordionSection from './components/accordion';
+import Footer from './components/footer';
 
 const LandingPage = () => {
   return (
     <div className='w-full bg-white font-body max-w-[1600px] m-auto px-4 md:px-8 lg:px-14'>
       <Header />
+      <Hero />
       <AccordionSection />
       <Footer />
     </div>
