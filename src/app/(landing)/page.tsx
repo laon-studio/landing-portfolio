ac17387e7225f0862a787b7269b154ad11330a40
byol-han <byolhandev@gmail.com>
2025-06-02 00:28:41 -0700

    feat: created animated counter

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,12 +1,14 @@
 import Header from './components/header';
 import Footer from './components/footer';
 import AccordionSection from './components/accordion';
+import CounterSection from './components/counter';
 
 const LandingPage = () => {
   return (
     <div className='w-full bg-white font-body max-w-[1600px] m-auto px-4 md:px-8 lg:px-14'>
       <Header />
       <AccordionSection />
+      <CounterSection />
       <Footer />
     </div>
   );
