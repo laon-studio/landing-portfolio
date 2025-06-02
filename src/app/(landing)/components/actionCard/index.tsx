9b6295816f6a0ef0a6210d00e41388684697b62c
byol-han <byolhandev@gmail.com>
2025-06-02 00:35:53 -0700

    fix: change name from section02 to actionCard

---
+++ src/app/(landing)/components/actionCard/index.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const PaymentButton = () => {
+  return (
+    <Link
+      href='/contact'
+      className='px-3 pb-[9px] pt-[9px] rounded-lg font-normal text-xs lg:text-sm tracking-[0.1em] block w-fit h-fit uppercase justify-center flex gap-2 items-center transition-all duration-300 text-[#fff] bg-black hover:opacity-90 group'
+    >
+      <span className='text-xs tracking-[0.1em] uppercase'>
+        start a payment
+      </span>
+      <span className='w-[10px] h-[10px] transition-transform duration-300 transform rotate-45 group-hover:-rotate-0 '>
+        <Image
+          src='/arrow_outward-white.svg'
+          alt='arrow_outward-white'
+          width={10}
+          height={10}
+          priority
+        />
+      </span>
+    </Link>
+  );
+};
+
+const ActionCard = () => {
+  return (
+    <div className='flex flex-col md:flex-row '>
+      <div className='relative hidden md:flex items-center justify-center p-8 aspect-[6/4] md:max-w-[460px] md:max-h-[460px] md:aspect-square w-full rounded-[30px] overflow-hidden'>
+        <Image
+          alt='CTA Background'
+          className='object-cover'
+          fill
+          sizes='100vw'
+          src='/CTA-BG2.jpg'
+        />
+      </div>
+      <div className='flex flex-col justify-center w-full rounded-[30px] bg-primary py-8 px-6  lg:px-28 gap-4 md:gap-8'>
+        <p className='text-xs tracking-[0.1em] uppercase'>
+          Get started in minutes
+        </p>
+        <h2 className='text-black text-4xl leading-4xl md:text-5xl md:leading-5xl pb-[120px] md:pb-0'>
+          Built to take your business further, faster.
+        </h2>
+        <PaymentButton />
+      </div>
+    </div>
+  );
+};
+export default ActionCard;
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,14 +1,14 @@
 import Header from './components/header';
 import Footer from './components/footer';
 import AccordionSection from './components/accordion';
-import Section02 from './components/section02';
+import ActionCard from './components/actionCard';
 
 const LandingPage = () => {
   return (
     <div className='w-full bg-white font-body max-w-[1600px] m-auto px-4 md:px-8 lg:px-14'>
       <Header />
       <AccordionSection />
-      <Section02 />
+      <ActionCard />
       <Footer />
     </div>
   );
