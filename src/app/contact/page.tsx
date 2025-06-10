80ca244e14fe961e63b889a1cc2894540e5f591a
sojeongsojeong-park <psj90972@naver.com>
2025-06-10 12:09:30 +0900

    fix: fix box ratio

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,8 +3,8 @@ import OptInForm from './components/opt-in-form';
 
 const ContactPage = () => {
   return (
-    <div className='flex px-8 pb-[60px] md:pb-[120px]'>
-      <div className='w-full bg-primary rounded-[30px] px-4 py-8 md:p-8 flex flex-col gap-4 justify-center'>
+    <div className='grid items-center w-full h-auto gap-0 md:min-h-[calc(100vh-164px)] md:max-h-[960px] grid-cols-1 md:grid-cols-7 pb-[60px] md:pb-[120px]'>
+      <div className='w-full h-full bg-primary rounded-[30px] px-4 py-8 md:p-8 flex flex-col gap-4 justify-center col-span-4'>
         <div className='flex gap-2 items-center'>
           <div className='block w-7 p-2 bg-black rounded-lg'>
             <Image
@@ -25,7 +25,7 @@ const ContactPage = () => {
           <OptInForm />
         </div>
       </div>
-      <div className='w-full p-8 bg-[#c0b395] rounded-[30px] hidden md:block'>
+      <div className='w-full h-full justify-center p-8 bg-[#c0b395] rounded-[30px] hidden md:block col-span-3'>
         <OptInForm />
       </div>
     </div>
